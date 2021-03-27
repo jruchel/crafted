9ec93bb7216b8b1614a0632624317dccf8a438f3
Jakub Ruchel <jruchel@gmail.com>
2021-03-27 16:33:33 +0100

    Changed product card layout

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,46 +1,51 @@
 <template>
   <v-sheet min-height="250" class="fill-height" color="transparent">
-  <v-lazy  transition="fade-transition" :v-model="visible" height="100%" width="100%"  :options="{
+    <v-lazy transition="fade-transition" :v-model="visible" height="100%" width="100%" :options="{
           threshold: .5
         }">
-  <v-card hover width="100%" height="100%" class="card-outter">
-    <v-card-title>
-      {{ product.name }} - {{ product.tagline }}
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-card-text>
-      {{ product.description }}
-      <br><br>
-      Pairs well with: <br>
-      <div id="food-pairing" v-for="food in this.product.food_pairing">
-        <span>-{{ food }}</span>
-      </div>
-    </v-card-text>
-    <v-spacer></v-spacer>
-    <v-card-actions class="card-actions">
-      <v-btn class="styled-button" v-if="typeof greenButton !== 'undefined'" depressed color="#81C784"
-             style="color: white"
-             @click="greenButtonClicked">{{
-          greenButton.text
-        }}
-      </v-btn>
-      <span style="margin-left: 5%">Price: ${{ product.price }}</span>
-      <v-btn class="styled-button" v-if="typeof redButton !== 'undefined'" depressed color="#e53935"
-             style="color: white"
-             @click="redButtonClicked">{{
-          redButton.text
-        }}
-      </v-btn>
+      <v-card hover width="100%" height="100%" class="card-outter">
+        <v-card-title>
+          {{ product.name }}
+          <v-spacer>
+          </v-spacer>
+          {{ currency.sign }}{{ product.price }}
+        </v-card-title>
+        <v-card-subtitle>
+          {{ product.tagline }}
+        </v-card-subtitle>
+        <v-divider></v-divider>
+        <v-card-text>
+          {{ product.description }}
+          <br><br>
+          Pairs well with: <br>
+          <div id="food-pairing" v-for="food in this.product.food_pairing">
+            <span>-{{ food }}</span>
+          </div>
+        </v-card-text>
+        <v-spacer></v-spacer>
+        <v-card-actions class="card-actions">
+          <v-btn class="styled-button" v-if="typeof greenButton !== 'undefined'" depressed color="#81C784"
+                 style="color: white"
+                 @click="greenButtonClicked">{{
+              greenButton.text
+            }}
+          </v-btn>
+          <v-btn class="styled-button" v-if="typeof redButton !== 'undefined'" depressed color="#e53935"
+                 style="color: white"
+                 @click="redButtonClicked">{{
+              redButton.text
+            }}
+          </v-btn>
 
-      <v-btn class="styled-button" v-if="typeof blueButton !== 'undefined'" depressed color="#0288d1"
-             style="color: white"
-             @click="blueButtonClicked">{{
-          blueButton.text
-        }}
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-  </v-lazy>
+          <v-btn class="styled-button" v-if="typeof blueButton !== 'undefined'" depressed color="#0288d1"
+                 style="color: white"
+                 @click="blueButtonClicked">{{
+              blueButton.text
+            }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-lazy>
   </v-sheet>
 </template>
 
