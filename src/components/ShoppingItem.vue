0f29989bad919b333058d0c82c95d4fea90be729
Jakub Ruchel <jruchel@gmail.com>
2021-03-27 11:16:26 +0100

    Changed the layout of the shopping item card

--- src/components/ShoppingItem.vue
+++ src/components/ShoppingItem.vue
@@ -1,17 +1,17 @@
 <template>
-    <v-card v-show="visible">
-      <v-card-title>{{ item.name }} - {{item.tagline}}
-        <v-spacer></v-spacer>
-        <v-btn color="#E53935" icon @click="deleteFromCart(item)">
-          <v-icon large>
-            mdi-minus-circle
-          </v-icon>
-        </v-btn>
-      </v-card-title>
-      <v-card-subtitle>{{ this.currency.sign }}{{ item.price }}</v-card-subtitle>
-      <v-card-text>{{ item.description }}</v-card-text>
+  <v-card v-show="visible">
+    <v-card-title>{{ item.name }}, {{ this.currency.sign }}{{ item.price }}
       <v-spacer></v-spacer>
-    </v-card>
+      <v-btn color="#E53935" icon @click="deleteFromCart(item)">
+        <v-icon large>
+          mdi-minus-circle
+        </v-icon>
+      </v-btn>
+    </v-card-title>
+    <v-card-subtitle>{{ item.tagline }}</v-card-subtitle>
+    <v-card-text>{{ item.description }}</v-card-text>
+    <v-spacer></v-spacer>
+  </v-card>
 </template>
 
 <script>
